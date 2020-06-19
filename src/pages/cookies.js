f683dd8201cfe6dbefada70030c0cb10db718ddd
abdulahiroble <abdulahi_2610@hotmail.com>
2020-06-19 19:40:37 +0200

    minor style changes

--- src/pages/cookies.js
+++ src/pages/cookies.js
@@ -3,65 +3,32 @@ import Layout from "../components/layout";
 
 const Cookies = () => (
   <Layout>
-    <h1
-      style={{
-        paddingLeft: "10px",
-      }}
-    >
-      Cookies
-    </h1>
-    <p
-      style={{
-        paddingLeft: "1em",
-      }}
-    >
-      THjemmesiden anvender "cookies". Cookies er en tekstfil, der gemmes på din
+    <h1>Cookies</h1>
+    <br />
+    <p>
+      Hjemmesiden anvender "cookies". Cookies er en tekstfil, der gemmes på din
       computer, mobiltelefon, tablet eller tilsvarende med det formål at
       genkende den, huske indstillinger, udfører statistik og målrette annoncer.
       Cookies kan ikke indeholde skadelig kode som f.eks virus.
     </p>
 
-    <p
-      style={{
-        paddingLeft: "1em",
-      }}
-    >
+    <p>
       Du kan slette eller blokerer cookies. Se hvordan her{" "}
       <a href="http://minecookies.org/cookiehandtering">
         http://minecookies.org/cookiehandtering
       </a>
     </p>
 
-    <h2
-      style={{
-        paddingLeft: "10px",
-      }}
-    >
-      Nødvendige cookies
-    </h2>
+    <h2>Nødvendige cookies</h2>
 
-    <p
-      style={{
-        paddingLeft: "1em",
-      }}
-    >
+    <p>
       Nogle cookies kræves for at hjemmesiden kan fungere. Hjemmesiden fungerer
       ikke korrekt uden disse cookies, og de er som standard aktiveret.
     </p>
 
-    <h2
-      style={{
-        paddingLeft: "10px",
-      }}
-    >
-      Google Analytics
-    </h2>
+    <h2>Google Analytics</h2>
 
-    <p
-      style={{
-        paddingLeft: "1em",
-      }}
-    >
+    <p>
       Disse cookies giver os mulighed for at tælle besøgende og trafikkilder, så
       vi kan måle og forbedre ydeevnen af vores hjemmeside. De hjælper os med at
       finde ud af, hvilke sider der er mest og mindst populære samt hvordan de
