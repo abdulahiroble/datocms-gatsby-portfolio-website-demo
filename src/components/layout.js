f7e7e9f16c80b2b77b5f399e6780a18d1c329a31
abdulahiroble <abdulahi_2610@hotmail.com>
2020-06-19 19:25:40 +0200

    setting up cookie banner

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,6 +5,7 @@ import PropTypes from "prop-types";
 import { Link } from "gatsby";
 import { StaticQuery, graphql } from "gatsby";
 import { HelmetDatoCms } from "gatsby-source-datocms";
+import CookieConsent from "react-cookie-consent";
 
 import "../styles/index.sass";
 
@@ -100,6 +101,27 @@ const TemplateWrapper = ({ children }) => {
               </div>
             </div>
             {children}
+            <CookieConsent
+              location="bottom"
+              buttonText="Accept Cookies"
+              cookieName="myAwesomeCookieName2"
+              style={{ background: "#2B373B" }}
+              buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+              expires={150}
+            >
+              Vi bruger cookies til, at få vores hjemmeside til at virke
+              ordentligt, personalisere indhold og reklamer, tilbyde funktioner
+              i forhold til sociale medier og analysere vores traffik. Vi deler
+              også information vedrørende din brug af vores hjemmeside med
+              analytiske samarbejdspartnere
+              <Link
+                to="/cookies"
+                style={{ textDecoration: "none", color: "yellow" }}
+              >
+                {" "}
+                Læs mere om cookies
+              </Link>
+            </CookieConsent>
           </div>
         </div>
       )}
